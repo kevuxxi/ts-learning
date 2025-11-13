12b4274833a5779f97d01a9fedfaae082f566c6f
kevin <kevinmpz10@icloud.com>
2025-11-13 12:11:57 -0300

    desafio 2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//caso donde la funcion no retorna nada 
+function saludo(nombre) {
+    console.log(`Hola ${nombre}`);
+}
 const saludar = (nombre) => {
     return `hola ${nombre}, bienvenido a TypeScript`;
 };
@@ -14,4 +18,18 @@ let datos = ['kevin', true, 19];
 console.log(datos);
 let lista = [1, 2, 3];
 console.log(lista);
+let nombre = 'kevin';
+let edad = 30;
+let esdesarrollador = true;
+function presentar(nombre, edad, dev) {
+    const rol = dev ? 'desarrollador' : 'aficionado';
+    return `hola soy ${nombre}, tengo ${edad} años de edad, soy ${rol}`;
+}
+console.log(presentar(nombre, edad, esdesarrollador));
+function calcular(peso, altura) {
+    let imc = peso / altura;
+    let formateImc = Math.round(imc * 100) / 100;
+    return formateImc;
+}
+console.log(calcular(61, 171));
 //# sourceMappingURL=index.js.map
--- proyecto-ts/src/index.ts
+++ proyecto-ts/src/index.ts
@@ -1,3 +1,10 @@
+
+//caso donde la funcion no retorna nada 
+function saludo(nombre: string): void {
+    console.log(`Hola ${nombre}`);
+}
+
+
 const saludar = (nombre: string): string => {
     return `hola ${nombre}, bienvenido a TypeScript`
 }
@@ -17,3 +24,27 @@ console.log(datos)
 
 let lista: number[] = [1, 2, 3]
 console.log(lista)
+
+
+
+let nombre: string = 'kevin';
+let edad: number = 30;
+let esdesarrollador: boolean = true;
+
+
+function presentar(nombre: string, edad: number, dev: boolean): string {
+    const rol = dev ? 'desarrollador' : 'aficionado'
+    return `hola soy ${nombre}, tengo ${edad} años de edad, soy ${rol}`
+}
+
+console.log(presentar(nombre, edad, esdesarrollador));
+
+
+
+function calcular(peso: number, altura: number): number {
+    let imc = peso / altura
+    let formateImc = Math.round(imc * 100) / 100;
+    return formateImc
+}
+
+console.log(calcular(61, 171));
